b8b8d921b87704dc1e612085aa855ab0de0b3c46
znorf <159936181+znorf@users.noreply.github.com>
2025-02-04 18:25:48 +0000

    pair creation software
    to be upscaled later

---
+++ pair_creator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  4 17:36:38 2025
+
+@author: Frederick de Naeyer
+"""
+import numpy as np
+
+file = open('top_10_rockstar.csv')
+data = np.genfromtxt('top_10_rockstar.csv', delimiter=',', dtype=str)
+
+pairs=np.array(['mvir_0','mvir_1','x0','y0','z0','x1','y1','z1'])
+for i in range(1,10):
+    for j in range(i+1,11):
+        newrow=np.array([data[i,0],data[j,0],data[i,1],data[i,2],data[i,3],data[j,1],data[j,2],data[j,3]])
+        pairs=np.vstack([pairs, newrow])
