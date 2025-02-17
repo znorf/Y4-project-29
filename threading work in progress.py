02436ad6b468f8fd92a132e0e25f1f71dbee032c
znorf <159936181+znorf@users.noreply.github.com>
2025-02-17 02:39:45 +0000

    Add files via upload

---
+++ threading work in progress.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 22:43:19 2025
+
+@author: Freddie
+"""
+
+import numpy as np
+import threading
+
+def create_p(start, end, split):
+    start=int(start)
+    end=int(end)
+    split=int(split)
+    
+    globals()['p_pairs_id_S'+str(split)]=''
+    globals()['p_pairs_m0_S'+str(split)]=''
+    globals()['p_pairs_m1_S'+str(split)]=''
+    
+    globals()['p_pairs_x0_S'+str(split)]=''
+    globals()['p_pairs_y0_S'+str(split)]=''
+    globals()['p_pairs_z0_S'+str(split)]=''
+    
+    globals()['p_pairs_x1_S'+str(split)]=''
+    globals()['p_pairs_y1_S'+str(split)]=''
+    globals()['p_pairs_z1_S'+str(split)]=''
+    
+    globals()['p_pairs_ds_S'+str(split)]=''
+    
+    for i in range(start,end):
+        locals()['p_pairs_id'+str(i)]=''
+        locals()['p_pairs_m0'+str(i)]=''
+        locals()['p_pairs_m1'+str(i)]=''
+        
+        locals()['p_pairs_x0'+str(i)]=''
+        locals()['p_pairs_y0'+str(i)]=''
+        locals()['p_pairs_z0'+str(i)]=''
+        
+        locals()['p_pairs_x1'+str(i)]=''
+        locals()['p_pairs_y1'+str(i)]=''
+        locals()['p_pairs_z1'+str(i)]=''
+        
+        locals()['p_pairs_ds'+str(i)]=''
+        for j in range(i+1,n+1):
+            #calcaulates seperation, ds
+            dx=float(data[i,1])-float(data[j,1])
+            dy=float(data[i,2])-float(data[j,2])
+            dz=float(data[i,3])-float(data[j,3])
+            ds=np.sqrt(dx**2+dy**2+dz**2)
+            if ds<=20 and ds>=3: #criteria for physical pairs
+                
+                pair_id=str(i)+';'+str(j) #pair id is combination of positions in list
+                #adds physical pair to list
+                newrow=np.array([data[i,0],data[j,0],data[i,1],data[i,2],data[i,3],data[j,1],data[j,2],data[j,3],ds,pair_id])
+                
+                locals()['p_pairs_id'+str(i)]=locals()['p_pairs_id'+str(i)]+','+pair_id
+                
+                locals()['p_pairs_m0'+str(i)]=locals()['p_pairs_m0'+str(i)]+','+str(data[i,0])
+                
+                locals()['p_pairs_m1'+str(i)]=locals()['p_pairs_m1'+str(i)]+','+data[j,0]
+                
+                locals()['p_pairs_x0'+str(i)]=locals()['p_pairs_x0'+str(i)]+','+data[i,1]
+                
+                locals()['p_pairs_y0'+str(i)]=locals()['p_pairs_y0'+str(i)]+','+data[i,2]
+                
+                locals()['p_pairs_z0'+str(i)]=locals()['p_pairs_z0'+str(i)]+','+data[i,3]
+                
+                locals()['p_pairs_x1'+str(i)]=locals()['p_pairs_x1'+str(i)]+','+data[j,1]
+                locals()['p_pairs_y1'+str(i)]=locals()['p_pairs_y1'+str(i)]+','+data[j,2]
+                locals()['p_pairs_z1'+str(i)]=locals()['p_pairs_z1'+str(i)]+','+data[j,3]
+                locals()['p_pairs_ds'+str(i)]=locals()['p_pairs_ds'+str(i)]+','+str(ds)
+
+    for i in range(start,end):
+        globals()['p_pairs_id_S'+str(split)]=globals()['p_pairs_id_S'+str(split)]+locals()['p_pairs_id'+str(i)]
+
+        globals()['p_pairs_m0_S'+str(split)]=globals()['p_pairs_m0_S'+str(split)]+locals()['p_pairs_m0'+str(i)]
+        
+        globals()['p_pairs_m1_S'+str(split)]=globals()['p_pairs_m1_S'+str(split)]+locals()['p_pairs_m1'+str(i)]
+        
+        globals()['p_pairs_x0_S'+str(split)]=globals()['p_pairs_x0_S'+str(split)]+locals()['p_pairs_x0'+str(i)]
+        
+        globals()['p_pairs_y0_S'+str(split)]=globals()['p_pairs_y0_S'+str(split)]+locals()['p_pairs_y0'+str(i)]
+        
+        globals()['p_pairs_z0_S'+str(split)]=globals()['p_pairs_z0_S'+str(split)]+locals()['p_pairs_z0'+str(i)]
+        
+        globals()['p_pairs_x1_S'+str(split)]=globals()['p_pairs_x1_S'+str(split)]+locals()['p_pairs_x1'+str(i)]
+        
+        globals()['p_pairs_y1_S'+str(split)]=globals()['p_pairs_y1_S'+str(split)]+locals()['p_pairs_y1'+str(i)]
+        
+        globals()['p_pairs_z1_S'+str(split)]=globals()['p_pairs_z1_S'+str(split)]+locals()['p_pairs_y1'+str(i)]
+        
+        globals()['p_pairs_ds_S'+str(split)]=globals()['p_pairs_ds_S'+str(split)]+locals()['p_pairs_ds'+str(i)]
+        
+        del locals()['p_pairs_id'+str(i)], locals()['p_pairs_m0'+str(i)], locals()['p_pairs_m1'+str(i)], locals()['p_pairs_x0'+str(i)], locals()['p_pairs_y0'+str(i)], locals()['p_pairs_z0'+str(i)], locals()['p_pairs_x1'+str(i)], locals()['p_pairs_y1'+str(i)], locals()['p_pairs_z1'+str(i)], locals()['p_pairs_ds'+str(i)]
+    
+    
+    
+    
+
+
+
+file = open('top_1000_rockstar.csv') #opens file containing top 10 highest mass haloes
+data = np.genfromtxt('top_1000_rockstar.csv', delimiter=',', dtype=str) #extracts data from file
+
+n=len(data[:,0])-1 #number of data values
+
+create_p(1, 500, 0)
+
+
+if __name__ =="__main__":
+    t0 = threading.Thread(target=create_p, args=(1,200,0))
+    t1 = threading.Thread(target=create_p, args=(201,400,1))
+    t2 = threading.Thread(target=create_p, args=(401,600,2))
+    t3 = threading.Thread(target=create_p, args=(601,800,3))
+
+    t0.start()
+    t1.start()
+    t2.start()
+    t3.start()
+    
+
+    t0.join()
+    t1.join()
+    t2.join()
+    t3.join()
+
+    print("Done!")
