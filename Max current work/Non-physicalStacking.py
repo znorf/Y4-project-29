a96ed726e2a5b34275c490767d3a37e8f34c779e
MaxMacd <159165243+MaxMacd@users.noreply.github.com>
2025-04-04 12:11:50 +0100

    Add files via upload

--- Max current work/Non-physicalStacking.py
+++ Max current work/Non-physicalStacking.py
@@ -1,3 +1,4 @@
+
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,7 +20,7 @@ def NValues():                                                                  
     D2 = data.iloc[cluster2,1:4].values.reshape(1, -1)                                  #Co-ords of cluster 2
     R1 = data.iloc[cluster1,4]/1000                                                     #Virial radius of cluster 1. /1000 to convert from h^-1 Kpc to h^-1 Mpc
     R2 = data.iloc[cluster2,4]/1000                                                     #Virial radius of cluster 2                                                                                 #This is the R_2D separation between the origional clusters in the physical pair. For testing purposes, this is set to 10 h^-1 Mpc
-    zslice = np.abs(D1[0,2]-D2[0,2])/2                                                          #set the +- slice thickness. For testing purposes this is set so that all data will be in the zslice.       
+    zslice = 2                                                         #set the +- slice thickness. For testing purposes this is set so that all data will be in the zslice.       
     length = ((D1[0,0]+1.5*XYZsep)-(D1[0,0]-0.5*XYZsep))/bins                             #length of each box in the histogram
     height = ((D1[0,1]+XYZsep)- (D1[0,1]-XYZsep))/bins                                    #height of each box in the histogram
     vol = length * height * zslice 
@@ -32,6 +33,7 @@ def Nenvironment(cluster1,cluster2):             #A function to find the indexes
     global ind1
     global ind2
     global mid
+    
     mid = (D1+ np.array([[XYZsep/2,0,0]]))/2
     ind1,ind2,indVir1,indVir2 = [],[],[],[]
 
@@ -51,9 +53,9 @@ def Nenvironment(cluster1,cluster2):             #A function to find the indexes
 
     for indices in ind:                                     #ind (index) is in a wierd format, this for loop turns a long list of numbers into a list where each new number is a new column.
         for i in indices:
+            
             if np.abs(D1[0,2]-data.iloc[i,3])< zslice and i not in indVir1 and i not in indVir2:     #This filters the haloes so that only those within z values +- a certain value from the first cluster in the non-physical pair are allowed. The second statement filters out all haloes within 2*Virial radius of the first and second clusters in the non-physical pair
-                ind1.append(i)
-     
+                ind1.append(i) 
     ind = tree.query_radius((D2- np.array([[XYZsep/2,0,0]])), r=(np.sqrt((XYZsep)**2+ XYZsep**2+ zslice**2)))    #repeat the steps above for the second cluster in the non-physical pair.            
     for indices in ind:
         for i in indices:
@@ -89,8 +91,10 @@ def Nplot():                                                                    
     '''
     plt.figure()
     plt.plot(xc1,yc1,'r.')
-    plt.plot(xc2,yc2,'b.')                                                                              
-    plt.plot(D1[0,0],D1[0,1],color = 'blue',marker='*',markersize=15)
+    plt.plot(xc2,yc2,'b.') 
+    plt.xlim(D1[0,0]-0.5*XYZsep, D1[0,0]+1.5*XYZsep)  
+    plt.ylim(D1[0,1]-XYZsep, D1[0,1]+XYZsep)                                                                           
+    plt.plot(D1[0,0],D1[0,1],color = 'orange',marker='*',markersize=15)
     plt.plot(D1[0,0]+XYZsep, D1[0,1],color = 'green',marker='*',markersize=15)    
     '''
 #end of functions
@@ -107,22 +111,24 @@ XYZ = data.iloc[:,1:4]
 bins = 100                                                                                                  #set the number of bins in the histogram
 Stack = np.zeros((bins,bins))                                                                               #create an array to contain the stacking of non-physical pairs
 i=0 
-select = 4000  #max 4608                                                                    #A while loop to create density arrays for all non-physical paris in the non-physical pair catalogue 'pairs'.         
+select = 800  #max 4608
+rand = [random.randint(0, (len(Npairs)/2)-1) for _ in range(select)]  
+                                                               #A while loop to create density arrays for all non-physical paris in the non-physical pair catalogue 'pairs'.         
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 #Funciton calling section    
-while i <len(Npairs)-select:  
+for i in rand:  
+    i = i*2
     cluster1,cluster2 = int(Npairs[i]), int(Npairs[i+1])                                          #take the first non-physical pair from the pair catalogue
     xc1,yc1,mc1,xc2,yc2,mc2,D1,D2,R1,R2,XYZsep,zslice,vol,cluster1,cluster2,theta,lims = NValues()
-    Nenvironment(cluster1,cluster2)  
-    #NonPhysrotation(random.randint(0, 628)/100)                                                       #Rotation function - random number between 0 and 2pi (approximately 6.28).
-    Nrotation(0)                                                                                       #Rotation of 0 degrees for testing purposes
+    Nenvironment(cluster1,cluster2)                                                      
+    Nrotation(random.randint(0, 628)/100)                                                                     #Rotation function - random number between 0 and 2pi (approximately 6.28)
     Nplot()                                                                 
-    i+=2    
+
   
 Filament = np.rot90(Stack/select*2)                                                               #The data has to be rotated by 90 degrees for plt.imshow()        
 plt.figure()
 plt.imshow(Filament,norm=LogNorm())                                                         #Plot the mass density histogram
-#plt.colorbar()
+plt.colorbar()
 plt.plot(np.array([25,75]),np.array([50,50]),color = 'red',marker='*',markersize=12)        #Red stars to show the location of the physical pair clusters
 plt.axis('scaled')
 end = time.time()
